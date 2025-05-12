8d8c6417ea4ca48f45a113a00f6e105c14add33d
Westle Robb <wrobb.debate@gmail.com>
2025-05-12 06:03:21 -0600

    Add texture imports for pipe and planes sheet assets; configure project settings
    - Imported pipe texture as a compressed 2D texture with specific parameters.
    - Added planes sheet texture with compression settings.
    - Updated project configuration file with application details, display settings, input mappings, and rendering method.
    - Added basic input and animations on input

---
+++ Scenes/Plane/Plane.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class Plane : CharacterBody2D
+{
+
+	const float GRAVITY = 800.0f;
+	const float FLIGHT_POWER = 400.0f;
+	[Export] private AnimationPlayer _anims;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		// Velocity returns a copy of the value so must be stored before editing
+		Vector2 velocity = Velocity;
+		if (Input.IsActionJustPressed("fly"))
+		{
+			GD.Print("flying!");
+			velocity.Y -= FLIGHT_POWER;
+			Velocity = velocity; // Set target Velocity
+			MoveAndSlide(); // Invoke MoveAndSlide to move based on Velocity
+
+			_anims.Play(name: "Power");
+		}
+		else
+		{
+			velocity.Y += GRAVITY * (float)delta; // Apply GRAVITY to target Velocity
+			Velocity = velocity; // Set target Velocity
+			MoveAndSlide(); // Invoke MoveAndSlide to move based on Velocity
+		}
+	}
+}
+
